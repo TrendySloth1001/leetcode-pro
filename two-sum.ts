d74e0afd5ddc62ef5af2b67e4970fae654276710
Nikhil Kumawat <133957092+TrendySloth1001@users.noreply.github.com>
2025-12-14 13:24:39 +0530

    first commit

---
+++ two-sum.ts
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/two-sum/submissions/1855194299/
+
+
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+    for (let i = 0; i< nums.length; i++){
+        const current = nums[i]
+        const complement = target - current;
+        if(map.has(complement)){
+            return[map.get(complement)!,i];
+        }
+        map.set(current,i);
+    }
+    return [];
+};
