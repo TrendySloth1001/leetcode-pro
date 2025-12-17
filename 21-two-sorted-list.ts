798be6d9c2c2b8f8504a540a0e3294b57a54f077
Nikhil Kumawat <133957092+TrendySloth1001@users.noreply.github.com>
2025-12-17 22:13:30 +0530

    wr4r34r

--- 21-two-sorted-list.ts
+++ 21-two-sorted-list.ts
@@ -20,16 +20,16 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
             list1 = list1.next;
         } else {
             current.next = list2;
-            list2 = list2.next;
+            list2 = list2.next; 
         }
         current = current.next;
     }
 
-    if (list1 !== null) {
+    if (list1 !== null) { 
         current.next = list1; 
     } else {
         current.next = list2;
     }
 
     return dummy.next;
-};
+};  
